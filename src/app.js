608db8e94cbd67c421c3811ab1e539bb014c04ce
cowang01 <g_cowan27@hotmail.com>
2019-06-17 18:10:26 +0100

    toDoList-MVP

--- src/app.js
+++ src/app.js
@@ -7,7 +7,17 @@ document.addEventListener('DOMContentLoaded', () => {
       list: [
         {detail: 'test1', done: false},
         {detail: 'test2', done: false}
-      ]
+      ],
+      newDetail: '',
+    },
+    methods: {
+      addToList: function(){
+        this.list.push({detail: this.newDetail, done: false})
+        this.newDetail = ''
+      },
+      itemDone: function(index){
+        this.list[index].done = true
+      }
     }
   })
 })
